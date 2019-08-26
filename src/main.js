85852b6e3152724abff3506da59489225b4803e1
twitf <qiangzaiying123@outlook.com>
2019-08-26 22:09:17 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {Button} from 'ant-design-vue';
+
+Vue.use(Button)
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
